0ec765f55b286dbd0e16c067d453ecb96513e00d
qzmalekuz <qzmalekuz@Zafars-MacBook-Air.local>
2025-12-02 09:23:21 +0530

    controller added

--- controller.js
+++ controller.js
@@ -1,3 +1,12 @@
+const { User, Game } = require("./models");
+const { userSchemaZ } = require("./types");
+const express = require("express");
+const bcrypt = require("bcrypt");
+
+app.use(express.json());
+
+const saltRounds = parseInt(process.env.SALT_ROUNDS);
+
 const signupController =  async (req, res) => {
 
     const parsed= userSchemaZ.safeParse(req.body);
@@ -26,9 +35,32 @@ const user = await User.create({username: username, password: hashed});
 } catch(err) {
     console.log(err);
 }
-    
 }
 
+const loginController = async (req, res) => {
+    const parsed = userSchemaZ.safeParse(req.body);
+    if(!parsed.success){
+        return res.status(400).json(parsed.error.errors);
+    }
+
+    const { username, password } = parsed.data;
+    const user = await User.findOne({username});
+    if(!user){
+        return res.status(400).json({
+            err: "User not found"
+        });
+    }
+    const match = bcrypt.compareSync(password, user.password);
+    if(!match){
+        return res.status(400).json({
+            err: "Incorrect password"
+        });
+    }
+}
+
+
+
 module.exports = {
-    signupController
+    signupController,
+    loginController
 }
